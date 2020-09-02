85387b495eb7e410ae5ce57ccab7211b809d88c4
Damilola Faseun <fashfired@gmail.com>
2020-09-02 09:22:36 +0100

    chore: Added sass to app

--- BuildForSDG/src/App.js
+++ BuildForSDG/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import SignUpStudent from './components/SignUpStudent'
-import "./styles.css";
+import SignUpTutor from './components/SignUpTutor'
+import Login from './components/Login'
+import "./css/app.scss";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Find a Tutor app</h1>
       <SignUpStudent />
+      <br/>
+      <br/>
+      <SignUpTutor />
+      <br />
+      <br />
+      <Login />
     </div>
   );
 }
