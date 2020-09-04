6ca9f2964b00a945b33f3af9c784a7b3a5f61cd1
Damilola Faseun <fashfired@gmail.com>
2020-09-04 19:38:23 +0100

    feat: Created a landing page

--- BuildForSDG/src/components/LandingPage.js
+++ BuildForSDG/src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import '../css/landing-page.scss'
 import SignUpStudent from './SignUpStudent';
 import SignUpTutor from './SignUpTutor';
 import Home from './Home'
@@ -8,6 +9,8 @@ import {
   Container,
   Row,
   Col,
+  Image,
+  Button,
 } from 'react-bootstrap'
 import {
   BrowserRouter as Router,
@@ -19,57 +22,81 @@ import {
 function LandingPage() {
 
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/">Signup</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Container>
-        <Row>
-          <Col>
-            {/* <Switch> */}
-              <Route path="/" render={() => (
-                <Home />
-              )}
+    <div className="wrapper">
+
+      <Router>
+        <div >
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/">Signup</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* <Switch> */}
+            <Route path="/" render={() => (
+              <Home />
+            )}
+            />
+            <Route path="/about" render={() => (
+              <About />
+            )}
+            />
+            <Route path="/login" render={() => (
+              <Login />
+            )}
+            />
+            <Route path="/student-signup" render={() => (
+              <SignUpStudent />
+            )}
+            />
+            <Route path="/tutor-signup" render={() => (
+              <SignUpTutor />
+            )}
+            />
+          {/* </Switch> */}
+        </div>
+        <Container>
+          <Row className>
+            <Col className="flex-display">
+              <h2>Find a Tutor</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+              <div className="signup-button-wrapper">
+                <Button 
+                  className="signup-button"
+                  variant="danger"
+                >
+                  STUDENT
+                </Button>
+                <Button
+                  className="signup-button"
+                  variant="danger"
+                >
+                  TUTOR
+                </Button>
+              </div>
+            </Col>
+            {/* <Col>
+              <Image
+                src={landingImage}
+                alt="landing page image"
+                width="900"
+                height="900"
               />
-              <Route path="/about" render={() => (
-                <About />
-              )}
-              />
-              <Route path="/login" render={() => (
-                <Login />
-              )}
-              />
-              <Route path="/student-signup" render={() => (
-                <SignUpStudent />
-              )}
-              />
-              <Route path="/tutor-signup" render={() => (
-                <SignUpTutor />
-              )}
-              />
-            {/* </Switch> */}
-          </Col>
-          <Col>
-            Image file
-          </Col>
-        </Row>
-      </Container>
-    </Router>
+            </Col> */}
+          </Row>
+        </Container>
+      </Router>
+    </div>
   )
 }
 
