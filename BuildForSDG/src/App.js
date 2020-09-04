dde0d835d167aee1fc7271db541d41ad9942a784
Damilola Faseun <fashfired@gmail.com>
2020-09-04 12:32:25 +0100

    chore: Render landing page on fake token

--- BuildForSDG/src/App.js
+++ BuildForSDG/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import SignUpStudent from './components/SignUpStudent'
 import SignUpTutor from './components/SignUpTutor'
 import Login from './components/Login'
 import "./css/app.scss";
+import "./css/signup-login.scss"
+import LandingPage from "./components/LandingPage";
+
 
 export default function App() {
+  const [page, setPage] = useState('signup')
+  // const [token, setToken] = useState('token')
+  const [token, setToken] = useState('')
+
+
+  if (!token) {
+    return (
+      <div>
+        <LandingPage />
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
       <h1>Find a Tutor app</h1>
